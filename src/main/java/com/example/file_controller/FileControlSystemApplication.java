3dda7bed97f526e4ae57015bdb29226f646d5b24
scui0452 <scui0452@uni.syndy.edu.au>
2025-06-30 13:55:10 +0800

    Initial commit

---
+++ src/main/java/com/example/file_controller/FileControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/file_controller/FileControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
